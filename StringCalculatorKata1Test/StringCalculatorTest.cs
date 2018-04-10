b1505c06517f0c22c7a119a91d4e3004009109b0
dewtheoboe <37633277+dewtheoboe@users.noreply.github.com>
2018-04-10 16:17:19 -0500

    Added 2 more Unit Tests

--- StringCalculatorKata1Test/StringCalculatorTest.cs
+++ StringCalculatorKata1Test/StringCalculatorTest.cs
@@ -17,5 +17,27 @@ namespace StringCalculatorKata1Test
             //Assert
             Assert.AreEqual(result, 0);
         }
+
+        [TestMethod]
+        public void OneStringShouldEqualItself()
+        {
+            //Arrange
+            StringCalculator testcalc = new StringCalculator();
+            //Act
+            int result = testcalc.AddString("4");
+            //Assert
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void Strings1and2ShouldEqual3()
+        {
+            //Arrange
+            StringCalculator testcalc = new StringCalculator();
+            //Act
+            int result = testcalc.AddString("1","2");
+            //Assert
+            Assert.AreEqual(result, 3);
+        }
     }
 }
