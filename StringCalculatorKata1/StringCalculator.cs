9f7f9d3c10d3d085ee75acbee42836fd060eaad9
dewtheoboe <37633277+dewtheoboe@users.noreply.github.com>
2018-04-10 15:52:13 -0500

    Initial Attempt, EmptyStringShouldEqual0 Test

---
+++ StringCalculatorKata1/StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata1
+{
+    public class StringCalculator
+    {
+        public int AddString(string A = "0", string B = "0", string C = "0")
+        {
+            int NumberA = int.Parse(A);
+            int NumberB = int.Parse(B);
+            int NumberC = int.Parse(C);
+            int res = NumberA + NumberB + NumberC;
+            return res;
+        }
+
+    }
+}
---
+++ StringCalculatorKata1Test/StringCalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorKata1;
+
+namespace StringCalculatorKata1Test
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void EmptyStringShouldEqual0()
+        {
+            //Arrange
+            StringCalculator testcalc = new StringCalculator();
+            //Act
+            int result = testcalc.AddString();
+            //Assert
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
